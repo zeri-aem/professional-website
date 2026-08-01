be536ebfe96a9f7db6269043f4bf5fc068d872e0
zeri-aem <malacaszeriaaron@gmail.com>
2026-08-01 16:34:50 +0800

    Added icons to use and styled contact and project

--- src/components/pages/home/ContactPreview.tsx
+++ src/components/pages/home/ContactPreview.tsx
@@ -1,4 +1,7 @@
 import Button from "../../common/Button";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+const ICON_SIZE = 32;
 
 function ContactPreview() {
   return (
@@ -10,6 +13,17 @@ function ContactPreview() {
 
       <Button>Email Me</Button>
       {/* List of other contacts */}
+      <div className="flex justify-start items-center gap-3 p-3 bg-[#a8dadc] rounded-2xl shadow-xl">
+        <a href="https://github.com/zeri-aem" target="_blank">
+          <FaGithub size={ICON_SIZE} />
+        </a>
+        <a href="https://github.com/zeri-aem" target="_blank">
+          <FaGithub size={ICON_SIZE} />
+        </a>
+        <a href="https://linkedin.com/in/zeri-aaron" target="_blank">
+          <FaLinkedin size={ICON_SIZE} />
+        </a>
+      </div>
     </section>
   );
 }
--- src/components/pages/projects/ProjectCard.tsx
+++ src/components/pages/projects/ProjectCard.tsx
@@ -12,14 +12,21 @@ function ProjectCard({ ...project }: ProjectCardProps) {
   return (
     <Link
       to="/projects"
-      className="flex flex-col md:max-w-80 lg:max-w-95 rounded-xl shadow border border-gray-200/30"
+      className="flex flex-col h-full md:max-w-80 lg:max-w-95 rounded-xl shadow border border-gray-200/30"
     >
-      <img
+      <div className="w-full h-52">
+        <img
+          src={project.image}
+          alt={project.title}
+          className="w-full h-full object-cover rounded-tr-xl rounded-tl-xl"
+        />
+      </div>
+      {/* <img
         src={project.image}
         alt={project.title}
-        className="md:w-full md:h-40 lg:w-full lg:h-60 object-cover rounded-tr-xl rounded-tl-xl"
-      />
-      <div className="flex flex-col gap-3 justify-between h-full p-3 bg-white rounded-bl-xl rounded-br-xl">
+        className="w-full h-40 md:h-40 lg:h-60 object-cover rounded-tr-xl rounded-tl-xl"
+      /> */}
+      <div className="flex flex-col flex-1 gap-3 justify-between h-full p-3 bg-white rounded-bl-xl rounded-br-xl">
         <div>
           <div className="flex justify-between items-center gap-2">
             <h2 className="font-bold flex-1 text-lg">{project.title}</h2>
